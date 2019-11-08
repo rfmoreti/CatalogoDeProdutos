40b2d134a25cf3902e7b49c3c4a5508ff9989cda
projetoeai <projeto.eai19@gmail.com>
2019-11-08 19:32:27 -0300

    Banco windows

--- ProductCatalog.Mobile/ProductCatalog.Mobile/DAO/StringConnection.cs
+++ ProductCatalog.Mobile/ProductCatalog.Mobile/DAO/StringConnection.cs
@@ -4,21 +4,33 @@ using System.Text;
 
 using PCLExt.FileStorage;
 using PCLExt.FileStorage.Folders;
-
+using Windows.Storage;
+using Xamarin.Forms;
 
 namespace ProductCatalog.Mobile.DAO
 {
     public class StringConnection
     {
+        string dbName = "MeuBanco.db";
         public string GetString()
         {
-            //cria uma pasta base local para o dispositivo
-            var pasta = new LocalRootFolder();
-            //cria o arquivo
-            var arquivo =
-                pasta.CreateFile("MeuBanco.db", CreationCollisionOption.OpenIfExists);
-            //abre o BD
-            return arquivo.Path;
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                string path = ApplicationData.Current.LocalFolder.Path + "\\" + dbName;
+                return path;
+            }
+            else
+            {
+                //cria uma pasta base local para o dispositivo
+                var pasta = new LocalRootFolder();
+                //cria o arquivo
+                var arquivo =
+                    pasta.CreateFile(dbName, PCLExt.FileStorage.CreationCollisionOption.OpenIfExists);
+                //abre o BD
+                return arquivo.Path;
+            }
+
+          
         }
 
     }
